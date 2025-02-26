2060cdb8ca7e68aba4b2fc65b95731f9598c3a90
Dmitry Andrusishin <andrusishindv@gmail.com>
2025-02-26 16:09:44 +0300

    step 4 - update App, add parameters and options to ref info

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,24 @@
 package hexlet.code;
 
+import java.io.File;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
+
 public class App implements Runnable {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File filepath1;
+
+    @Parameters(index = "0", paramLabel = "filepath2", description = "path to second file")
+    private File filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
