ba5690f252af7d43d80d2160cad00bb539f08bc0
Dmitry Andrusishin <andrusishindv@gmail.com>
2025-04-14 21:46:56 +0300

    App.java reworked - removed file reading. Add Differ.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,5 @@
 package hexlet.code;
 
-import java.io.File;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
@@ -10,69 +7,38 @@ import picocli.CommandLine.Option;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Runnable {
+public class App implements Callable<Integer> {
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
-    private File filepath1;
+    private Path filepath1;
 
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
-    private File filepath2;
+    private Path filepath2;
 
     @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     private String format;
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(Arrays.toString(args));
-//        System.out.println(Paths.get("./app/src/main/test/resourses/f1.json").toAbsolutePath().normalize());
-
-
-        String readFilePath1 = args[0];
-        Path path1 = Paths.get(readFilePath1).toAbsolutePath().normalize();
-
-//        System.out.println(path1);
-//        path1 = "\\\\wsl.localhost\\Ubuntu\\home\\smthwd\\java-project-71"
-
-        String readFilePath2 = args[1];
-        Path path2 = Paths.get(readFilePath2).toAbsolutePath().normalize();
-
-//        System.out.println(path2);
-
-        if (!Files.exists(path1)) {
-            throw new Exception("File '" + path1 + "' does not exist");
+    @Override
+    public Integer call() throws Exception {
+        if (!Files.exists(filepath1)) {
+            throw new Exception("File '" + filepath1 + "' does not exist");
         }
 
-        if (!Files.exists(path2)) {
-            throw new Exception("File '" + path2 + "' does not exist");
+        if (!Files.exists(filepath2)) {
+            throw new Exception("File '" + filepath2 + "' does not exist");
         }
 
-        // Читаем файл
-        String content1 = Files.readString(path1);
-        String content2 = Files.readString(path2);
+        var compareResult = Differ.generate(filepath1, filepath2, format);
 
-        // Выводим содержимое
-        System.out.println(content1);
-        System.out.println(" ");
-        System.out.println(content2);
-
-        new CommandLine(new App()).execute(args);
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        Map<?, ?> j1 = mapper.readValue(content1, Map.class);
-        Map<?, ?> j2 = mapper.readValue(content2, Map.class);
-
-        System.out.println(j1);
-        System.out.println(j2);
+        return 0;
     }
 
-    @Override
-    public void run() {
-//        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+import java.nio.file.Path;
+
+public class Differ {
+    public static String generate(Path path1, Path path2, String format) {
+        return "asd";
+    }
+}
